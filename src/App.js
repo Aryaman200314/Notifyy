35c96faf112e526f076c6b7670949028b7f5099d
Aryaman Sharma <200314arya@gmail.com>
2024-10-15 23:56:18 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home.js';
 import Login from './pages/Login/Login.js';
 import SignUp from './pages/SignUp/SignUp.js';
 
-const routes = (
-  <Routes>
-    <Route path='/dashboard' element={<Home/>} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/signup' element={<SignUp />} />
-  </Routes>
-);
-
 function App() {
   return (
     <Router>
-      {routes}
+      <Routes>
+        {/* Default route redirecting to /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* Your other routes */}
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        
+        {/* Optional: Catch-all route to handle undefined routes */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
     </Router>
   );
 }
